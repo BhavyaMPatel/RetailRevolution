41501a8a29b8394781b8238475bbf3295b54b947
BhavyaMPatel <bhavyapatel4012@gmail.com>
2023-08-26 22:24:39 +0530

    getting responce from backend 👍

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,12 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile,BackgroundTasks
 from flask import Flask
 from flask_cors import CORS
 from fastapi.middleware.cors import CORSMiddleware
 import pandas as pd
 import joblib
+import csv
+import codecs
+
 preprocessor = joblib.load('preprocessor.pkl')
 loaded_model = joblib.load('model.pkl')
 
@@ -26,20 +29,27 @@ app.add_middleware(
 def read_root():
     return {"Hello": "World"}
 
-@app.get("/data")
-def read_data():
-    df = pd.read_csv('da.csv')
-    new_data_transformed = preprocessor.transform(df)
+@app.post("/data")
+async def read_data(background_tasks: BackgroundTasks,file:UploadFile=File(...)):
+    csvReader = csv.DictReader(codecs.iterdecode(file.file, 'utf-8'))
+    # df =  csv.DictReader(codecs.iterdecode(contents, 'utf-8'))
+    data_list = [row for row in csvReader]
+    
+    new_data = pd.DataFrame(data_list)
+
+    new_data_transformed = preprocessor.transform(new_data)
     new_predictions = loaded_model.predict(new_data_transformed)
     top_selling_indices = new_predictions.argsort()[-10:][::-1]
-    top_selling_stockcodes = df.loc[top_selling_indices, 'StockCode']
+    top_selling_stockcodes = new_data.loc[top_selling_indices, 'StockCode']
     top_selling_quantities = new_predictions[top_selling_indices]
     top_selling_items = []
     for stockcode, quantity in zip(top_selling_stockcodes, top_selling_quantities):
         item = {'StockCode': stockcode, 'PredictedQuantity': float(quantity)}
         top_selling_items.append(item)
 
+    background_tasks.add_task(file.file.close)
 
-    return top_selling_items
+    # return top_selling_items
+    return {"high": top_selling_items}
 
 
--- frontend/src/UserDashBoard/DashBoardPage.jsx
+++ frontend/src/UserDashBoard/DashBoardPage.jsx
@@ -2,35 +2,30 @@ import React, { useEffect,useState } from 'react'
 
 export default function DashBoardPage() {
 
-const [Data,SetData] = useState([]);
+const [File,SetFile] = useState("");
+var formData = new FormData();
 
-useEffect(()=>{
-  fetch('http://localhost:8000/data')
-  .then(response => response.json())
-  .then(data => {
-  // Update the options state
-  SetData(data);
+async function SendFile(e){
+  e.preventDefault();
+      
+  formData.append("file",File);
+  const response =  await fetch('http://localhost:8000/data',{
+  method: 'POST',
+  body: formData
+  })
+
+  const data = await response.json();
   console.log(data);
-  });
-},[]);
-
-function SendFile(e){
-
-    e.preventDefault()
-    console.log('Send File')
 
  }
   return (
     <>
         <div className='mt-20'>
-            
             <form className='m-5' onSubmit={SendFile}>
-                
-                <label className="p-3 block mb-2 text-sm font-medium text-gray-900 dark:text-white" for="file_input">Upload Excel</label>
-                <input className="p-3 block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" id="file_input" type="file"/>
-                <button type="submit" className='m-2 p-3 bg-blue-200 font-Poppins rounded-md hover:p-4'>Send..</button>
+                <label className="p-3 block mb-2 text-sm font-medium text-gray-900 dark:text-white" htmlFor="file_input">Upload Excel</label>
+                <input  onChange={(e)=>SetFile(e.target.files[0])}  className="p-3 block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" name='file' id="file" type="file"/>
+                <button type="submit"className='m-2 p-3 bg-blue-200 font-Poppins rounded-md hover:p-4'>Send..</button>
             </form>
-
         </div>
     </>
   )
